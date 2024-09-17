cad37b8bed43eb30f1f9ca487883a02fa61d4b8d
Pradeep Pandey <90238672+UtechP-40@users.noreply.github.com>
2024-09-17 13:22:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here!
 
+let list = document.getElementById("infi-list");
+
+
+
+let x =1;
+while(true){
+    let li = document.createElement("li")
+    li.innerText = `Item ${x}`
+    list.append(li)
+    if(x==10){
+        break
+    }
+    x++;
+}
+
+list.addEventListener('scroll',()=>{
+    console.log("page is being scrolled")
+    let li = document.createElement("li")
+    li.innerText = `Item ${x}`
+    list.append(li)
+    x++;
+})
